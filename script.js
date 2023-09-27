0a490b39c660393cb88c74a0efd0c8e46ecf7423
aeross <andrewpratama22@gmail.com>
2023-09-27 16:39:55 +1000

    .

---
+++ script.js
@@ -0,0 +1,14 @@
+const DATA = [
+    {
+        id: "1",
+        name: "string",
+        address: "string",
+        telp: "string",
+    },
+    {
+        id: "2",
+        name: "string",
+        address: "string",
+        telp: "string",
+    },
+];
