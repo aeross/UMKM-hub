16cc543b417f21127bf56dd6c4d15c3d4682a5e0
aeross <andrewpratama22@gmail.com>
2023-09-27 17:58:51 +1000

    Database class DONE

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
-const DATA = [
-    {
-        id: "1",
-        name: "string",
-        address: "string",
-        telp: "string",
-    },
-    {
-        id: "2",
-        name: "string",
-        address: "string",
-        telp: "string",
-    },
-];
+// supplier data
+let DATA = [];
+// supplier id
+let id = 0;
+
+
+class Database {
+    #DATA = [];
+    #id = 0;
+
+    get getData() {
+        return this.#DATA;
+    }
+
+    /** PRIVATE METHODS */
+
+    #getSupplierById(id) {
+        for (let i = 0; i < this.#DATA.length; i++) {
+            if (this.#DATA[i].id === id) {
+                return this.#DATA[i];
+            }
+        }
+    }
+
+    #getIndexById(id) {
+        for (let i = 0; i < this.#DATA.length; i++) {
+            if (this.#DATA[i].id === id) {
+                return i;
+            }
+        }
+    }
+
+    /** PUBLIC METHODS */
+
+    /* adds a new supplier to DATA. */
+    add(name, address, email, telp, contactName, tagline, desc, MoQ, priceRange, category) {
+        this.#id++;
+        this.#DATA.push({
+            id: this.#id, name, address, email, telp, contactName, tagline, desc, MoQ, priceRange, category 
+        });
+    }
+
+    /* deletes the supplier based on id from DATA. */
+    delete(id) {
+        let indexToRemove = this.#getIndexById(id);
+        this.#DATA.splice(indexToRemove, 1);
+    }
+}
