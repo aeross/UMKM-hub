a6dacf6e1c60788243a8c91ad3d70dc0fb559581
aeross <andrewpratama22@gmail.com>
2023-09-27 19:35:56 +1000

    added Favourites class

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// supplier data
-let DATA = [];
-// supplier id
-let id = 0;
-
-
 class Database {
     #DATA = [];
     #id = 0;
@@ -12,25 +6,24 @@ class Database {
         return this.#DATA;
     }
 
-    /** PRIVATE METHODS */
-
-    #getSupplierById(id) {
+    getSupplierById(id) {
         for (let i = 0; i < this.#DATA.length; i++) {
             if (this.#DATA[i].id === id) {
                 return this.#DATA[i];
             }
         }
+        throw new Error(`Error: id=${id} doesn't exist`);
     }
 
-    #getIndexById(id) {
+    getIndexById(id) {
+        console.log(this.#DATA);
         for (let i = 0; i < this.#DATA.length; i++) {
             if (this.#DATA[i].id === id) {
                 return i;
             }
         }
+        throw new Error(`Error: id=${id} doesn't exist`);
     }
-
-    /** PUBLIC METHODS */
 
     /* adds a new supplier to DATA. */
     add(name, address, email, telp, contactName, tagline, desc, MoQ, priceRange, category) {
@@ -42,7 +35,51 @@ class Database {
 
     /* deletes the supplier based on id from DATA. */
     delete(id) {
-        let indexToRemove = this.#getIndexById(id);
+        let indexToRemove = this.getIndexById(id);
         this.#DATA.splice(indexToRemove, 1);
+    }
+}
+
+class Favourites {
+    #FAVES = [];
+    #id = 0;
+
+    get getFavourites() {
+        return this.#FAVES;
+    }
+
+    getSupplierById(id) {
+        for (let i = 0; i < this.#FAVES.length; i++) {
+            if (this.#FAVES[i].id === id) {
+                return this.#FAVES[i];
+            }
+        }
+        throw new Error(`Error: id=${id} doesn't exist`);
+    }
+
+    getIndexById(id) {
+        for (let i = 0; i < this.#FAVES.length; i++) {
+            if (this.#FAVES[i].id === id) {
+                return i;
+            }
+        }
+        throw new Error(`Error: id=${id} doesn't exist`);
+    }
+
+    /** adds a new supplier to FAVES.
+     * parameters:
+     * data: Database object
+     * dataId: supplier id from data to be added to FAVES
+     */ 
+    add(data, dataId) {
+        let supplier = data.getSupplierById(dataId);
+        this.#id++;
+        this.#FAVES.push(supplier);
+    }
+
+    /* deletes the supplier based on id from FAVES. */
+    delete(id) {
+        let indexToRemove = this.getIndexById(id);
+        this.#FAVES.splice(indexToRemove, 1);
     }
 }
